71ed892b09e3dc6649a9ae906bcc0d12e696ab7b
logbran <logan.r.brand@gmail.com>
2018-05-31 11:49:48 -0400

    Updated number of enemies
    Changed the number of enemies so there's not an ungodly number of old men chasing you down

--- Ambitious-Boi/Assets/_Complete-Game/Scripts/BoardManager.cs
+++ Ambitious-Boi/Assets/_Complete-Game/Scripts/BoardManager.cs
@@ -144,7 +144,7 @@ namespace Completed
             //LayoutObjectAtRandom (foodTiles, foodCount.minimum, foodCount.maximum);
 
             //Determine number of enemies based on current level number, based on a logarithmic progression
-            int enemyCount = 10;
+            int enemyCount = 5;
 			
 			//Instantiate a random number of enemies based on minimum and maximum, at randomized positions.
 			LayoutObjectAtRandom (enemyTiles, enemyCount, enemyCount);
