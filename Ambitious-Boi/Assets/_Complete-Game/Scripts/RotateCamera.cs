07121cba7d095b4c303049e19465412337ad8daa
logbran <logan.r.brand@gmail.com>
2018-05-31 11:18:02 -0400

    Updated all files
    Added procedural generation, map, player and AI spawns

---
+++ Ambitious-Boi/Assets/_Complete-Game/Scripts/RotateCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            float currentRotation = 90;
+            Camera.main.transform.eulerAngles = new Vector3(0, 0, currentRotation);
+            //transform.Rotate(Vector3.forward, currentRotation);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            float currentRotation = -90;
+            transform.eulerAngles = new Vector3(0, 0, currentRotation);
+        }
+    }
+}
